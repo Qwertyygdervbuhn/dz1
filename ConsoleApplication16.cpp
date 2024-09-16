8c3b330deebe676c1d0f3132b5acabf7ab7e6b1e
Alexander Dm. <dabl21291@gmail.com>
2024-09-16 19:18:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication16.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	int arr[5]{ 1,2,3,4,5 };
+	for (int a = 0; a < 5; a++) {
+		cout << arr[a] << "\t";
+	}
+
+	for (int i = 5; i > -1; i--) {
+		cout << arr[i] << endl;
+	}
+	return 0;
+}
+
+
